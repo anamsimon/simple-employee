4c8cb7441033190fab9c7054f2c943fe980497c2
anam <anam.simon@gmail.com>
2021-06-17 21:02:34 +0800

    init

---
+++ Employee.Web.MVC/Data/EmployeeWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Employee.Models;
+
+namespace Employee.Web.Data
+{
+    public class EmployeeWebContext : DbContext
+    {
+        public EmployeeWebContext (DbContextOptions<EmployeeWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Employee.Web.MVC/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string FirstName { get; set; }
+
+    }
+}
